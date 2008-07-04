2f907425b9507a4cf8e229a89f960683555ba793
c704271 <c704271@medus-c704.uibk.ac.at>
2008-07-04 18:03:58 +0200

    added test sequence

---
+++ PulseSequences/protected/test_sequence.py
@@ -0,0 +1,45 @@
+# TEst sequence for unittests for the server
+
+<VARIABLES>
+det_time=self.set_variable("float","det_time",100000.000000,0.01,2e7)
+</VARIABLES>
+
+# The save form specifies which data will be saved and how, when a scan is performed.
+# If this is omitted a standard form is used
+<SAVE FORM>
+  .dat   ;   %1.2f
+  PMTcounts;   1;sum; 		(1:N);		%1.0f
+</SAVE FORM>
+
+# Here the sequence can override program parameters. Syntax follows from "Write Token to Params.vi"
+<PARAMS OVERRIDE>
+AcquisitionMode fluorescence
+DOasTTLword 1
+Cycles 1
+</PARAMS OVERRIDE>
+
+<SEQUENCE>
+if det_time == 100000.0:
+    raise RuntimeError("Variable not set")
+print det_time
+test_global("Juhu")
+test_include("123")
+for i in range(1):
+    rf_pulse(1,0,1,"carrier1", is_last=False)
+    rf_pulse(1,0,1,"carrier2", is_last=False)
+    ttl_pulse("1", 1000, is_last=False)
+ttl_pulse(["3", "5"],500)
+for i in range(1):
+    ttl_pulse("6", 700)
+
+# if switch729: rf_set729(freq729,power729)
+# else: rf_set729(freq729,-100)
+# if switchRaman: rf_setRaman(freqRaman,powerRaman)
+# else: rf_setRaman(freqRaman,-100)
+
+# incl.PMTDetection(det_time,gl_cam_time,no_lasers=True)
+</SEQUENCE>
+
+<AUTHORED BY LABVIEW>
+1
+</AUTHORED BY LABVIEW>
