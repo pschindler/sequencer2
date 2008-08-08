fbe2a10b2ae024cab8d17a5834891e3b6cdfe8cf
boerge <devnull@localhost>
2008-08-08 17:44:21 +0200

    added PMTDetection include file and a test server boerge

---
+++ PulseSequences/includes2/PMTDetection.py
@@ -0,0 +1,40 @@
+def PMTDetection(pmt_detect_wait):
+    global return_str
+    return_str += "PM Count, 2;"
+
+
+    # ttl_pulse(name, duration, starttime=0.0)
+
+    PMT_trigger_length = 1 
+
+    ttl_pulse("PMT trigger", PMT_trigger_length)
+
+    ttl_pulse("PMT trigger", PMT_trigger_length, start_time=pmt_detect_wait)
+
+
+    seq_wait(50)
+
+
+
+#    seq_wait(55)
+
+
+#    seq_wait(25)
+
+#    ttl_pulse("866sw", 25, 25, is_last=False)
+
+#    ttl_pulse("397 dop/det", 25, 37.5, is_last=False)
+    
+#    seq_wait(25)
+
+#    ttl_pulse("PB dummy", 75, 50, is_last=False)
+
+
+
+#    ttl_pulse("PMT trigger",30, 120, is_last=False)
+#    ttl_pulse("866sw",30, 25, is_last=True)
+ 
+
+    #ttl_set("397sw",1)
+
+ 
