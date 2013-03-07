d233bd94b66caea30f5f769bcf0babe7e5304a71
Thomas W. Holleis <thomas@fsfe.org>
2013-03-07 16:27:33 +0100

    Update PMTDetection.py to a more meaningful default version.

--- PulseSequences/includes2/PMTDetection.py
+++ PulseSequences/includes2/PMTDetection.py
@@ -1,7 +1,9 @@
-def PMTDetection(pmt_detect_length):
+def PMTDetection(pmt_det_length):
     """generate PMT detection event"""
+
     current_pm_counts = get_return_var("PM Count")
     if current_pm_counts == None:
         current_pm_counts = 0
-    add_to_return_list("PM Count",current_pm_counts + 2)
-    ttl_pulse("15",300, is_last=True)
+    add_to_return_list("PM Count", current_pm_counts + 2)
+    ttl_pulse("PM_Gate", 100, start_time=0, is_last=False)
+    ttl_pulse("PM_Gate", 100, start_time=pmt_det_length, is_last=True) 
